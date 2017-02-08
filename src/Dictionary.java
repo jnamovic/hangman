714b315d3c23f1d5fb3ff475f5060eef89b8dccc
jonathannamovic <jonathannamovic@CHS-A104-DSK-01.CHS.chatham-nj.org>
2017-02-08 11:43:48 -0500

    create project

---
+++ src/Dictionary.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Dictionary {
+
+	private File file =new File("NauticalPirateterms.txt");
+	private String word;
+	public Dictionary(){
+		try
+		{
+			Scanner scan=new Scanner(file);
+			for(int i=0; i<(int)(Math.random()*500000000);i++){
+				if(scan.hasNextLine())
+					word=scan.nextLine();
+				else
+					scan=new Scanner(file);
+			}
+		}	
+		catch(FileNotFoundException s){
+			System.out.println("help");
+		}
+		
+	}
+	public String getWord(){
+		return word;
+	}
+	public String getNewWord(){
+		try
+		{
+			Scanner scan=new Scanner(file);
+			for(int i=0; i<(int)(Math.random()*500000000);i++){
+				if(scan.hasNextLine())
+					word=scan.nextLine();
+				else
+					scan=new Scanner(file);
+			}
+		}	
+		catch(FileNotFoundException s){
+			System.out.println("help");
+		}
+		return word;
+	}
+}
---
+++ src/HangmanView.java
@@ -0,0 +1,22 @@
+
+public class HangmanView {
+
+	HangmanModel model;
+	String word;
+	public HangmanView(HangmanController controller ){
+		
+	}
+	
+	public void startUp(HangmanModel model, String word){
+		this.model=model;
+		this.word=word;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
