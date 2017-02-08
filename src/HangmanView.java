4bbbea5bb9ce9361e5d0f72cc6eab2ddf5cd7ac1
paulmalatesta <paulmalatesta@CHS-A104-DSK-02.CHS.chatham-nj.org>
2017-02-08 12:33:09 -0500

    started guess, you're going to rerail this train

--- src/HangmanView.java
+++ src/HangmanView.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
 
 public class HangmanView {
 
 	HangmanModel model;
 	String word,guessArea;
-	public HangmanView(HangmanController controller ){
-		
+	Gallows execute;
+	private final double wid = 500;
+	private final double ht = 650;
+	char guessedLetter;
+	public HangmanView(HangmanController controller){
+		execute = new Gallows(wid, ht);
 	}
 	
 	public void startUp(HangmanModel model, String word){
@@ -15,6 +20,26 @@ public class HangmanView {
 	public String getGuessArea()
 	{
 		return guessArea;
+	}
+	public void guess(ArrayList<Integer> g)
+	{
+		if(g.get(0)==-1)
+		{
+			execute.drawNextPart();
+		}
+		else
+		{
+			while(g.size()>=1){
+				if(g.get(0)==0)//for first letter mess ups
+				{
+					guessArea = guessedLetter + guessArea.substring(1);
+					g.remove(0);
+					break;
+				}
+				if(g.get(0)==)
+				guessArea = guessArea.substring(0, g.get(0))+guessedLetter + guessArea.substring(g.get(0));
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
