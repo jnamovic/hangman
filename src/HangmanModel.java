21c7890f8bfec726e43a733463c1a5d9930db897
jonathannamovic <jonathannamovic@CHS-A104-DSK-01.CHS.chatham-nj.org>
2017-02-08 12:29:35 -0500

    fixed some model things

--- src/HangmanModel.java
+++ src/HangmanModel.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class HangmanModel {
 
 	HangmanView view;  // the view
-	String word="heeevcedeefeaeck";
-	Dictionary dict;
-	int guesses;
+	private String word="heeevcedeefeaeck";
+	private Dictionary dict;
+	private int guesses=0;
+	private final int MAX_GUESS=5;
 	public HangmanModel(HangmanView view){
 		this.view=view;
 		dict=new Dictionary();	
-		System.out.println(guessMade('l'));
 		setUpGame();
 		
 	}
@@ -19,18 +19,37 @@ public class HangmanModel {
 		view.startUp(this,word);
 	}
 	
-	public ArrayList<Integer> guessMade(char letter){
-		ArrayList<Integer> morgrius=new ArrayList<Integer>();
-		for(int i=0;i<word.length();i++){
-			if(word.charAt(i)==letter){
-				morgrius.add(i);
+	public void guessMade(char letter){
+			ArrayList<Integer> morgrius=new ArrayList<Integer>();
+			for(int i=0;i<word.length();i++){
+				if(word.charAt(i)==letter){
+					morgrius.add(i);
+				}
 			}
-		}
-		if(morgrius.size()==0){
-			guesses++;
-			morgrius.add(-1);
-		}
-		return morgrius;
+			if(morgrius.size()==0){
+				guesses++;
+				morgrius.add(-1);
+			}
+			
+			if (view.getGuessArea().equals(word)) {
+				//view.winNotification();
+				return;
+			}
+			
+			if (guesses<MAX_GUESS) {
+				//view.loseNotification();
+				return;
+			}
+		
 	}
 	
+//	public boolean hasNotLost(){
+//		return(guesses<MAX_GUESS);
+//	}
+//	public boolean hasNotWon(){
+//		return(view.getGuessArea().equals(word));
+//	}
+//	
 }
+
+
