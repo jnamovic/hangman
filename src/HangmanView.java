6f0ca159172f14e3519776470e7cc98db3c2a440
paulmalatesta <paulmalatesta@CHS-LMC-DSK-02.CHS.chatham-nj.org>
2017-02-17 10:31:22 -0500

    adding javadocs

--- src/HangmanView.java
+++ src/HangmanView.java
@@ -12,6 +12,7 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import acm.graphics.GCompound;
+import acm.graphics.GImage;
 import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GPoint;
@@ -26,13 +27,14 @@ public class HangmanView extends GCompound implements MouseListener, ActionListe
 	Gallows execute;
 	LetterArea areaOfLetters;
 	JLabel feedback,score;
-	GImage boat;
+	GImage ship;
 	GLetter gobj ;
 	JButton button;
 	private final double WID = 150;
 	private final double HT = 150;
 	private final double LETTERX = 350;
 	private final double LETTERY = 100;
+	private final double SHIP_OFFSET = 75;
 	char guessedLetter;
 	public HangmanView(HangmanController controller){
 		execute = new Gallows(WID, HT);
@@ -45,8 +47,16 @@ public class HangmanView extends GCompound implements MouseListener, ActionListe
 		game.addMouseListeners(this);
 		game.addActionListeners(this);
 		catchResizeEvents();
+		ship = new GImage("boat.png");
+		ship.scale(.25,.25);
 	}
-	
+	/**
+	 * Sets up game by removing all items on screen and making/re-adding all of them
+	 * @param model
+	 * @param word
+	 * @param win
+	 * @param lose
+	 */
 	public void startUp(HangmanModel model, String word,int win, int lose){
 		this.removeAll();
 		this.model=model;
@@ -60,9 +70,13 @@ public class HangmanView extends GCompound implements MouseListener, ActionListe
 		add(areaOfLetters);
 		areaOfLetters.displayedWord.setColor(Color.black);
 		game.add(this);
-		
+		add(ship,0,getHeight()-SHIP_OFFSET);
 	}
-	
+	/**
+	 * Updates the word and hangman upon guess, sets gletter unguessable
+	 * @param guess
+	 * @param good
+	 */
 	public void guess(String guess, boolean good)
 	{
 		if(good){
@@ -74,6 +88,9 @@ public class HangmanView extends GCompound implements MouseListener, ActionListe
 			gobj.onceGuessed(false);
 		}
 	}
+	/**
+	 * Game is won, allows game to be reset and congratulates player
+	 */
 	public void winNotification()
 	{
 		
@@ -83,16 +100,21 @@ public class HangmanView extends GCompound implements MouseListener, ActionListe
 		areaOfLetters.displayedWord.setColor(Color.green);
 		
 	}
-	
+	/**
+	 * What is displayed when game is lost
+	 * @param word
+	 */
 	public void loseNotification(String word)
 	{
-		message = "Hughhhh";
-		feedback.setText(message + " you lost");
+		message = "Hughhhh you lost";
+		feedback.setText(message);
 		areaOfLetters.wordUpdate(word);
 		areaOfLetters.displayedWord.setColor(Color.red);
 		button.setText("New Game");
 	}
-	
+	/**
+	 * Returns which character was clicked
+	 */
 	public void mouseClicked(MouseEvent e) {
 		
 		GObject obj = areaOfLetters.giveChar(e.getX(),e.getY());
@@ -103,7 +125,9 @@ public class HangmanView extends GCompound implements MouseListener, ActionListe
 		model.guessMade(gobj.getLetter());	
 		
 	}
-	
+	/**
+	 * Makes a new game, adding a loss if you hadn't finished the game before
+	 */
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("New Game")) {
 			model.setUpGame();
@@ -114,6 +138,9 @@ public class HangmanView extends GCompound implements MouseListener, ActionListe
 		}
 	}
 	private double width,height;
+	/**
+	 * Resizes scalable elements
+	 */
 	private void catchResizeEvents() {
 		width = game.getWidth();
 		height = game.getHeight();
